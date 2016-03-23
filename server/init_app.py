20fd255695ce023b363f7f82435eacbdf2fffe7f
artur-bekasov <artur.bekasov@gmail.com>
2016-03-23 20:58:20 +0000

    Add init_app() function
    ...to be used instead of executing code on import

---
+++ server/init_app.py
@@ -0,0 +1,6 @@
+from api import api
+
+def init_app(app, config):
+	app.config.from_pyfile(config)
+	app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
+	app.register_blueprint(api, url_prefix='/api')
