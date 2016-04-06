bc3a083da29e58283322bb4bfdd1fc8c5c67861d
artur-bekasov <artur.bekasov@gmail.com>
2016-04-06 21:43:13 +0100

    Fix debug.py

--- server/debug.py
+++ server/debug.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
 import sys
-from server import init_app
+from app import app
+from init_app import init_app
 
-app = init_app(sys.argv[1])
+init_app(app, sys.argv[1])
 
 with app.app_context():
     app.run(debug=True)
